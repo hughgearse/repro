8eecdcaee5d92362f05876ca2bac89d50de9c761
des <des@icandriveatractor.com>
2020-01-29 11:04:09 +0100

    None

--- src/main/scala/Test.scala
+++
@@ -1,8 +0,0 @@
-import scala.scalajs.js
-import scala.scalajs.js.annotation.JSImport
-
-@JSImport("./test", JSImport.Namespace)
-@js.native
-object Test extends js.Object {
-	val x: String = js.native
-}
